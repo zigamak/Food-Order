c8b71a83f9e58599b3c63649ad8eddd03d878fce
zigamak <zigamakinde@gmail.com>
2022-03-22 15:44:21 +0100

    Home page and Admin Design

---
+++ admin/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ziga Foods -Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!--Menu Section Starts-->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Admin</a>
+                </li>
+                <li>
+                    <a href="#">Category</a>
+                </li>
+                <li>
+                    <a href="#">Food</a>
+                </li>
+                <li>
+                    <a href="#">Order</a>
+                </li>
+            </ul>
+        </div>
+       
+    </div>
+    <!--Menu Section Ends-->
+
+    <!--Main Content Section Starts-->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard</h1>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Categories
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--Menu Content Section Ends-->
+
+    <!--Footer Section Starts-->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center"> 2022 All rights reserved. Ziga Foods. Developed by - <a href="#">ZIGA </a></p>
+        </div>
+    </div>
+    <!--Footer Section Ends-->
+</body>
+</html>
