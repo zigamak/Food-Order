e2eec1ea7938d1d917130bdc8758b782ac3af3b8
zigamak <zigamakinde@gmail.com>
2022-04-04 15:15:13 +0100

    Login Design

--- admin/index.php
+++ admin/index.php
@@ -17,6 +17,13 @@ include("partials/menu.php");
 <div class="main-content">
         <div class="wrapper">
             <h1>Dashboard</h1>
+            
+    <?php
+        if(isset($_SESSION['login'])){
+            echo $_SESSION['login'];
+            unset($_SESSION['login']);
+        }
+    ?>
             <div class="col-4 text-center">
                 <h1>5</h1>
                 <br>
--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?php
+include('config/constants.php');
+?>
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,9 +13,62 @@
     <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
-    <div class="login">
-        <h1>Login</h1>
-        <p>Created by <a href="www.zigamak.github.io">Ziga</a> </p>
+    <?php
+        if(isset($_SESSION['login'])){
+            echo $_SESSION['login'];
+            unset($_SESSION['login']);
+        }
+    ?>
+    
+    <div class="container">
+        <div class="screen">
+            <div class="screen__content">
+                <form class="login"><h1>Login</h1>
+                    <div class="login__field">
+                        <i class="login__icon fas fa-user"></i>
+                        <input type="text" class="login__input" placeholder="Username" name="username">
+                    </div>
+                    <div class="login__field">
+                        <i class="login__icon fas fa-lock"></i>
+                        <input type="password" class="login__input" placeholder="Password" name="password">
+                    </div>
+                    <button class="button login__submit" name="submit">
+                        <span class="button__text">Log In Now</span>
+                        <i class="button__icon fas fa-chevron-right"></i>
+                    </button>				
+                </form>
+                
+            </div>
+            <div class="screen__background">
+                <span class="screen__background__shape screen__background__shape4"></span>
+                <span class="screen__background__shape screen__background__shape3"></span>		
+                <span class="screen__background__shape screen__background__shape2"></span>
+                <span class="screen__background__shape screen__background__shape1"></span>
+            </div>		
+            <p class="text-center">Created by <a href="www.zigamak.github.io">Ziga</a> </p> 
+        </div>
+        
+       
     </div>
+    
 </body>
 </html>
+
+<?php
+if (isset($_POST['submit'])){
+    $username= $_POST['username'];
+    $password=$_POST['password'];
+
+    $sql="SELECT * FROM tbl_admin WHERE username='$username' AND password= '$password'";
+
+    $res=mysqli_query($conn, $sql);
+    $count=mysqli_num_rows($res);
+    if ($count==1){
+        $_SESSION['login']="<div class='success'>Login Succesful.</div>";
+        header('location:'.SITEURL.'admin/');
+    }else{
+        $_SESSION['login']="<div class='error'>Username or Password not Correct</div>";
+        header('location:'.SITEURL.'admin/login.php');
+    }
+}
+?>
