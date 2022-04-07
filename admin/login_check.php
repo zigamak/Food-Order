9e82b2b056459d1586257b56e8b7885e44b7422b
zigamak <zigamakinde@gmail.com>
2022-04-07 14:39:51 +0100

    Authorization

--- admin/login_check.php
+++ admin/login_check.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_SESSION['user']))
+if (!isset($_SESSION['user']))
 {
     $_SESSION['no-login-message']="<div class='error'>Please Log in</div>";
     header('location:'.SITEURL.'admin/admin.php');
